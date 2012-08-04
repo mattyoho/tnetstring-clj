449eaedeec61ccb67a127704889c4c46c4227468
Matt Yoho <mby@mattyoho.com>
2012-08-03 19:56:55 -0700

    Parse other things, not yet dicts

--- src/tnetstring/core.clj
+++ src/tnetstring/core.clj
@@ -1,13 +1,27 @@
 (ns tnetstring.core
-  (:use [clojure.string :only [split]]))
+  (:use [clojure.string :only [split blank?]]))
+
+(declare parse)
+
+(defn- parse-list [str the-list]
+  (if (not (blank? str))
+    (let [[element str] (parse str)]
+      (recur str (conj the-list element)))
+    the-list))
 
 (def parsers
   { \# #(Integer/parseInt %)
     \^ #(Float/parseFloat %)
-    \' #(identity %)})
+    \! (fn [s] (if (or (= "true" s) (= "false" s))
+                       (Boolean/valueOf s)
+                       (throw (IllegalArgumentException. "Not a boolean"))))
+    \~ (fn [s] (if (not (blank? s))
+           (throw (IllegalArgumentException. "Nil does not have a body"))))
+    \, #(identity %)
+    \] #(parse-list % [])})
 
 (defn- parse-length [str]
-  (let [[length-str remain] (split str #":")]
+  (let [[length-str remain] (split str #":" 2)]
     [(Integer/parseInt length-str) remain]))
 
 (defn- payload [str len]
@@ -17,7 +31,7 @@
           parser       (fn [] (get parsers payload-type))]
       [((parser) payload-str), remain]))
 
-(defn parse-tnetstring
+(defn parse
   "Parses a tnetstring"
   [str]
   (let [[payload-len payload-str] (parse-length str)]
--- test/tnetstring/core_test.clj
+++ test/tnetstring/core_test.clj
@@ -4,16 +4,31 @@
 
 (deftest parse-a-number
   (testing "Parsing a single number"
-    (is (= [2, ""] (parse-tnetstring "1:2#")))
-    (is (= [14, ""] (parse-tnetstring "2:14#")))))
+    (is (= [2, ""] (parse "1:2#")))
+    (is (= [14, ""] (parse "2:14#")))))
 
 (deftest parse-a-float
   (testing "Parsing a floating point number (Who says you can't compare floats for equality?)"
-    (is (= [(float 2.5), ""] (parse-tnetstring "3:2.5^")))
-    (is (= [(float 3.14), ""] (parse-tnetstring "4:3.14^")))))
+    (is (= [(float 2.5), ""] (parse "3:2.5^")))
+    (is (= [(float 3.14), ""] (parse "4:3.14^")))))
+
+(deftest parse-booleans
+  (testing "Parsing a boolean value"
+    (is (= [true, ""] (parse "4:true!")))
+    (is (= [false, ""] (parse "5:false!")))
+    (is (thrown? IllegalArgumentException (parse "5:flase!")))))
+
+(deftest parse-nil
+  (testing "Parsing a nil"
+    (is (= [nil, ""] (parse "0:~")))
+    (is (thrown? IllegalArgumentException (parse "1:n~")))))
+
+(deftest parse-list
+  (testing "Parsing a list of elements"
+    (is (= [[12345 67890 "xxxxx"] ""] (parse "24:5:12345#5:67890#5:xxxxx,]")))))
 
 (deftest parse-a-string
   (testing "Parsing an empty string"
-    (is (= ["", ""] (parse-tnetstring "0:'"))))
+    (is (= ["", ""] (parse "0:,"))))
   (testing "Parsing a string"
-    (is (= ["foo", ""] (parse-tnetstring "3:foo'")))))
+    (is (= ["foo", ""] (parse "3:foo,")))))
