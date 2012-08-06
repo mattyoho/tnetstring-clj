b7cf985dca287fa19654a9f334e8c0f6ec67c248
Matt Yoho <mby@mattyoho.com>
2012-08-06 01:12:59 -0400

    Update project.clj and change to MIT license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tnetstring "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Parser and encoder for TNetstring representation of Clojure basic types"
+  :url "http://github.com/mattyoho/tnetstring-clj"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/mit-license.php/"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
